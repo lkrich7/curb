900f5dc868b49120db4914f401b7b1289fcc6d2c
lkrich7 <lkrich7@gmail.com>
2024-06-03 23:01:00 +0800

    feat: 默认权限解析器改为只解析path和query string，不读取body

--- curb-core/src/main/java/curb/core/DefaultPermissionResolver.java
+++ curb-core/src/main/java/curb/core/DefaultPermissionResolver.java
@@ -1,10 +1,11 @@
 package curb.core;
 
 import curb.core.model.Permission;
+import curb.core.util.StringUtil;
 import curb.core.util.UrlCodec;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
+import java.util.Objects;
 
 /**
  * 默认请求权限对象解析器实现
@@ -14,23 +15,24 @@ public class DefaultPermissionResolver implements PermissionResolver {
     @Override
     public Permission resolve(HttpServletRequest request, CurbAccessConfig config, Object handler) {
         String path = config.getPath();
-        if (path == null || path.isEmpty()) {
+        if (StringUtil.isBlank(path)) {
             path = request.getRequestURI();
         }
-
         Permission.Builder builder = Permission.builder().path(path);
 
-        Enumeration<String> names = request.getParameterNames();
-        while (names.hasMoreElements()) {
-            String name = names.nextElement();
-            String[] values = request.getParameterValues(name);
-            if (values != null) {
-                for (String value : values) {
-                    builder.addParam(name, UrlCodec.encodeUtf8(value));
-                }
-            }
-        }
-
+        // 解析参数
+        StringUtil.splitToStream(request.getQueryString(), "&")
+                .map(StringUtil::trimToNull)
+                .filter(Objects::nonNull)
+                .forEach(s -> {
+                    int index = s.indexOf('=');
+                    if (index <= 0) {
+                        return;
+                    }
+                    String key = s.substring(0, index);
+                    String value = UrlCodec.decodeUtf8(s.substring(index + 1));
+                    builder.addParam(key, value);
+                });
         return builder.build();
     }
 
