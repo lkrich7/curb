84a32306c379c9c6497e600f5c83d3f03138237f
lkrich7 <lkrich7@gmail.com>
2025-03-26 00:11:51 +0800

    refactor: curb-core移除guava依赖

--- curb-core/src/main/java/curb/core/util/SetUtil.java
+++ curb-core/src/main/java/curb/core/util/SetUtil.java
@@ -1,17 +1,20 @@
 package curb.core.util;
 
-import com.google.common.collect.Sets;
-
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public enum SetUtil {
     ;
 
     public static <T> Set<T> intersection(Set<T> set1, Set<T> set2) {
-        return Sets.intersection(set1, set2);
+        Set<T> ret = new LinkedHashSet<>(set1);
+        ret.retainAll(set2);
+        return ret;
     }
 
     public static <T> Set<T> difference(Set<T> set1, Set<T> set2) {
-        return Sets.difference(set1, set2);
+        Set<T> ret = new LinkedHashSet<>(set1);
+        ret.removeAll(set2);
+        return ret;
     }
 }
